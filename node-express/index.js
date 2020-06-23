c85370b9f7e9457138b267b71f9babdd180c5e73
Owaish Kalim <owaishkalim@gmail.com>
2020-06-23 12:45:25 +0530

    Assignment-1

--- node-express/index.js
+++ node-express/index.js
@@ -1,9 +1,11 @@
-const express = require('express'),
-       http = require('http');
+const express = require('express');
+const http = require('http');
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,7 +14,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
-
+app.use('/promotions', promoRouter); 
+app.use('/leader', leaderRouter); 
 
 app.use(express.static(__dirname + '/public'));
 
